b3480d0871a1c78a4a91f21bb90a7d5e5f852193
jason-nordheim <jason.nordheim@gmail.com>
2020-08-03 14:43:28 -0600

    styled navbar

--- components/nav.js
+++ components/nav.js
@@ -2,17 +2,38 @@ import Link from 'next/link'
 
 const Nav = () => {
   return (
-    <nav>
-      <ul style={{listStyle: 'none'}}>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-      </ul>
-    </nav>
-  )
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about"><a>About</a></Link>
+          </li>
+        </ul>
+      </nav>
+
+      <style jsx>{`
+        ul {
+          background: #333;
+          color: #fff;
+          list-style: none;
+          display: flex;
+        }
+        ul li {
+          font-size: 18px;
+          margin-right: 20px;
+        }
+        ul li a {
+          color: #fff;
+          text-decoration: none;
+        }
+      `}</style>
+    </>
+  );
 }
 
 export default Nav 
