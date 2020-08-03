e7038d3c300abec429ac363262c4eddfe9e76b71
jason-nordheim <jason.nordheim@gmail.com>
2020-08-03 15:21:45 -0600

    used layout component in index page and added api request for data

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,28 @@
-import Nav from '../components/nav'
 
-export default function Home() {
+
+import Layout from "../components/layout";
+import Prices from "../components/prices";
+
+
+const Home = ({ bpi }) => {
+  console.log(bpi.USD);
   return (
-    <div>
-      <Nav /> 
+    <Layout>
       <h1>Welcome to BitzPrice</h1>
-    </div>
-  )
-}
+      <Prices bpi={bpi} /> 
+    </Layout>
+  );
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://api.coindesk.com/v1/bpi/currentprice.json");
+  const data = await res.json();
+
+  console.log("data", data);
+
+  return {
+    props: { bpi: data.bpi },
+  };
+};
+
+export default Home;
